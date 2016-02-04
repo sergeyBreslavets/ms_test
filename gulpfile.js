a7be586a0c0cf3bbabd9fc2b4f7f39219f20c43e
sergeyBreslavets <venom.bren@gmail.com>
2016-02-04 10:52:39 +0400

    update teml for start

--- gulpfile.js
+++ gulpfile.js
@@ -1,182 +1,234 @@
 'use strict';
 
-// include gulp
-var gulp       = require('gulp');
-
-// include plug-ins
-var changed        = require('gulp-changed');
-var imagemin       = require('gulp-imagemin');
-var jshint         = require('gulp-jshint');
-var concat         = require('gulp-concat');
-var stripDebug     = require('gulp-strip-debug');
-var uglify         = require('gulp-uglify');
-var autoprefix     = require('gulp-autoprefixer');
-var cssnano        = require('gulp-cssnano');
-var sass           = require('gulp-sass');
-var notify         = require("gulp-notify");
-
-var spritesmith = require('gulp.spritesmith');
-
-
-//src file
-var htmlSrc        = './src/html/*.html';
-var sassSrc        = './src/sass/styles.scss';
-var imagesSrc      = './src/images/**/*';
-var jsSrc          = [     
-                        'bower_components/modernizr/modernizr.js',
-                        'bower_components/jquery/dist/jquery.js',
-                        
-                        'bower_components/bootstrap-sass/assets/javascripts/bootstrap/transition.js',
-                        'bower_components/bootstrap-sass/assets/javascripts/bootstrap/alert.js',
-                        'bower_components/bootstrap-sass/assets/javascripts/bootstrap/button.js',
-                        'bower_components/bootstrap-sass/assets/javascripts/bootstrap/carousel.js',
-                        'bower_components/bootstrap-sass/assets/javascripts/bootstrap/collapse.js',
-                        'bower_components/bootstrap-sass/assets/javascripts/bootstrap/dropdown.js',
-                        'bower_components/bootstrap-sass/assets/javascripts/bootstrap/modal.js',
-                        'bower_components/bootstrap-sass/assets/javascripts/bootstrap/tooltip.js',
-                        'bower_components/bootstrap-sass/assets/javascripts/bootstrap/popover.js',
-                        'bower_components/bootstrap-sass/assets/javascripts/bootstrap/scrollspy.js',
-                        'bower_components/bootstrap-sass/assets/javascripts/bootstrap/tab.js',
-                        'bower_components/bootstrap-sass/assets/javascripts/bootstrap/affix.js',
-                        
-                        'bower_components/waterwheelcarousel/js/jquery.waterwheelCarousel.js',
-
-                       './src/scripts/*.js'
-                    ];
+var gulp          = require('gulp'),
+    watch         = require('gulp-watch'),
+    prefixer      = require('gulp-autoprefixer'),
+    uglify        = require('gulp-uglify'),
+    sass          = require('gulp-sass'),
+    sourcemaps    = require('gulp-sourcemaps'),
+    // rigger        = require('gulp-rigger'),  //
+    cssmin        = require('gulp-cssnano'),
+    imagemin      = require('gulp-imagemin'),
+    pngquant      = require('imagemin-pngquant'),
+    rimraf        = require('rimraf'),
+    browserSync   = require("browser-sync"),
+    notify        = require("gulp-notify"),
+    concat        = require('gulp-concat'),
+    stripDebug    = require('gulp-strip-debug'),
+    spritesmith   = require('gulp.spritesmith'),
+    reload        = browserSync.reload;
 
 
-//src target
-var htmlTarget           = './www/';
-var sassTarget           = './www/assets/styles';
-var jsTarget             = './www/assets/scripts/';
-var fontBootstrapTarget  = './www/assets/fonts/bootstrap/';
-var imagesTarget         = './www/assets/images';
-var opensansTarget       = './www/assets/fonts/open-sans/';
+
+var path = {
+    build: { //Тут мы укажем куда складывать готовые после сборки файлы
+        html:    'www/',
+        js:      'www/assets/js/',
+        css:     'www/assets/css/',
+        images:  'www/assets/images/',
+        fonts:   'www/assets/fonts/',
+        fontBs:  'www/assets/fonts/bootstrap/',
+        sprite:  'www/assets/images/sprite/',
+        spriteScss: 'src/style/' 
+    },
+    src: { //Пути откуда брать исходники
+        html:    'src/html/*.html', //Синтаксис src/*.html говорит gulp что мы хотим взять все файлы с расширением .html
+        js:  [     
+                 'bower_components/modernizr/modernizr.js',
+                 'bower_components/jquery/dist/jquery.js',
+                
+                 'bower_components/bootstrap-sass/assets/javascripts/bootstrap/transition.js',
+                 'bower_components/bootstrap-sass/assets/javascripts/bootstrap/alert.js',
+                 'bower_components/bootstrap-sass/assets/javascripts/bootstrap/button.js',
+                 'bower_components/bootstrap-sass/assets/javascripts/bootstrap/carousel.js',
+                 'bower_components/bootstrap-sass/assets/javascripts/bootstrap/collapse.js',
+                 'bower_components/bootstrap-sass/assets/javascripts/bootstrap/dropdown.js',
+                 'bower_components/bootstrap-sass/assets/javascripts/bootstrap/modal.js',
+                 'bower_components/bootstrap-sass/assets/javascripts/bootstrap/tooltip.js',
+                 'bower_components/bootstrap-sass/assets/javascripts/bootstrap/popover.js',
+                 'bower_components/bootstrap-sass/assets/javascripts/bootstrap/scrollspy.js',
+                 'bower_components/bootstrap-sass/assets/javascripts/bootstrap/tab.js',
+                 'bower_components/bootstrap-sass/assets/javascripts/bootstrap/affix.js',
+        
+
+                 'src/js/main.js'
+              ]   ,//В стилях и скриптах нам понадобятся только main файлы
+        style:   'src/style/styles.scss',
+        images:  'src/images/**/*.*', //Синтаксис images/**/*.* означает - взять все файлы всех расширений из папки и из вложенных каталогов
+        fonts:   'src/fonts/**/*.*',
+        fontsBs: 'bower_components/bootstrap-sass/assets/fonts/bootstrap/*.*',
+        fontsAwesome: 'bower_components/components-font-awesome/fonts/*.*' ,
+        sprite:   'src/sprite/*.*'
+    },
+    watch: { //Тут мы укажем, за изменением каких файлов мы хотим наблюдать
+        html:    'src/html/*.html',
+        js:      'src/js/**/*.js',
+        style:   'src/style/**/*.scss',
+        images:  'src/images/**/*.*',
+        fonts:   'src/fonts/**/*.*',
+        sprite:  'src/sprite/*.*'
+    },
+    clean: './www/assets'
+};
+
+var config = {
+    server: {
+        baseDir: "./www"
+    },
+    tunnel: true,
+    host: 'gulp_zero',
+    port:  8080,
+    logPrefix: "Frontend_Devil"
+};
+
 // tasks 
 
-gulp.task('copyopensans', function() {
-    gulp.src('./bower_components/open-sans/fonts/*/*')
-        .pipe(gulp.dest(opensansTarget));
+gulp.task('sprites', function() {
+   var spriteData = 
+        gulp.src(path.src.sprite) // путь, откуда берем картинки для спрайта
+            .pipe(spritesmith({
+                imgName: 'sprite.png',
+                cssName: 'sprite.scss',
+            }))
+
+    spriteData.css.pipe(gulp.dest(path.build.spriteScss))
+    spriteData.img.pipe(gulp.dest(path.build.sprite))
+    .pipe(notify({
+            title: 'sprite',
+            message: 'sprite Complide'
+
+        }));
 });
 
-gulp.task('copyfont', function() {
-    gulp.src([ './bower_components/bootstrap-sass/assets/fonts/bootstrap/*', 
-               './bower_components/components-font-awesome/fonts/*'
-             ])
-        .pipe(gulp.dest(fontBootstrapTarget))
-         .pipe(notify({
-            title: 'fontIconBootstrap',
-            message: 'copy Complide'
 
-    }));
 
-});
-
-// JS hint task
-gulp.task('jshint', function() {
-    gulp.src('./src/scripts/*.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'))
-           .pipe(notify("jshint-complete the work!"));
-});
-// minify new images
-gulp.task('imagemin', function() {
-    gulp.src(imagesSrc)
-        .pipe(changed(imagesTarget))
-        .pipe(imagemin())
-        .pipe(gulp.dest(imagesTarget))
-         .pipe(notify({
-            title: 'imagemin',
-            message: 'imagemin-complete the work!'
-    }));
-});
-// minify new or changed HTML pages
-gulp.task('htmlpage', function() {
-    gulp.src(htmlSrc)
-        .pipe(changed(htmlTarget))
-       
-        .pipe(gulp.dest(htmlTarget))
-         .pipe(notify({
+// task html
+gulp.task('html:build', function () {
+    gulp.src(path.src.html) //Выберем файлы по нужному пути
+        // .pipe(rigger()) //Прогоним через rigger
+        .pipe(gulp.dest(path.build.html)) //Выплюнем их в папку build  
+        .pipe(notify({
             title: 'html',
-            message: 'html-complete the work!'
-    }));
+            message: 'html build Complide'
+
+        }))
+        .pipe(reload({stream: true})); //И перезагрузим наш сервер для обновлений
+      
 });
-// JS concat, strip debugging and minify
-gulp.task('scripts', function() {
-   gulp.src(jsSrc)
-        .pipe(concat('script.js'))
+
+gulp.task('js:build', function () {
+    gulp.src(path.src.js) //Найдем наш main файл
+        .pipe(concat('main.js'))
         .pipe(stripDebug())
         .pipe(uglify())
+        .pipe(gulp.dest(path.build.js)) //Выплюнем готовый файл в build
+        .pipe(notify({
+            title: 'js',
+            message: 'js build Complide'
 
-    .pipe(gulp.dest(jsTarget))
-    .pipe(notify({
-            title: 'scripts',
-            message: 'scripts-complete the work!'
-    }));
+        }))
+        .pipe(reload({stream: true})); //И перезагрузим сервер
 });
 
-
-gulp.task('sass', function () {
-   
-  gulp.src(sassSrc) 
-  .pipe(notify({
+gulp.task('style:build', function () {
+    gulp.src(path.src.style) //Выберем наш main.scss
+        .pipe(sourcemaps.init()) //То же самое что и с js
+        .pipe(sass()) //Скомпилируем
+        .pipe(prefixer()) //Добавим вендорные префиксы
+        .pipe(cssmin()) //Сожмем
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(path.build.css)) //И в build
+        .pipe(notify({
             title: 'sass',
-            message: 'start!'
-    }))
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefix('last 2 versions'))
-    .pipe(cssnano())
-    .pipe(gulp.dest(sassTarget))
-    .pipe(notify({
-            title: 'sass',
-            message: 'sass-complete the work!'
-    }));
+            message: 'sass build Complide'
+
+        }))
+        .pipe(reload({stream: true}));
 });
 
+gulp.task('image:build', function () {
+    gulp.src(path.src.images) //Выберем наши картинки
+        .pipe(imagemin({ //Сожмем их
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()],
+            interlaced: true
+        }))
+        .pipe(gulp.dest(path.build.images)) //И бросим в build
+         .pipe(notify({
+            title: 'images',
+            message: 'images compress Complide'
 
-
-
-gulp.task('sprites', function() {
-
-  var spriteData = gulp.src('./src/img/*.png').pipe(spritesmith());
-
-  spriteData.img
-    .pipe(gulp.dest('img.png'));
-
-  spriteData.css
-    .pipe(gulp.dest('css.css'));
+        }))
+        .pipe(reload({stream: true}));
 });
 
+gulp.task('fonts:build', function() {
+    gulp.src(path.src.fonts)
+        .pipe(gulp.dest(path.build.fonts))
+         .pipe(notify({
+            title: 'fonts',
+            message: 'fonts copy Complide'
 
-
-gulp.task('watch', ['imagemin', 'sass', 'htmlpage',  'scripts'],  function() {
-
-   gulp.watch('./src/html/*.html', function() {
-    gulp.run('htmlpage');
-    });
-    gulp.watch('./src/scripts/*.js', function() {
-        gulp.run('jshint', 'scripts');
-    });
-    gulp.watch('./src/sass/{,*/}*.{scss,sass}', function() {
-        gulp.run('sass');
-    });
-    gulp.watch('./src/images/*', function() {
-        gulp.run('imagemin');
-    });
-
+        }));
 });
 
+gulp.task('fontsbs:build', function() {
+    gulp.src([path.src.fontsBs, path.src.fontsAwesome])
+        .pipe(gulp.dest(path.build.fontBs))
+         .pipe(notify({
+            title: 'fonts bs',
+            message: 'fonts bs copy Complide'
 
-
-/// gulp default
-gulp.task('default', function() {
- 
-        gulp.run('htmlpage');
-        gulp.run('jshint', 'scripts');
-        gulp.run('styles');
-        gulp.run('sass');
-        gulp.run('imagemin'); 
-
+        }));
 });
 
+     
 
+
+
+gulp.task('build', [
+    'sprites',
+    'html:build',
+    'js:build',
+    'style:build',
+    'fonts:build',
+    'fontsbs:build',
+    'image:build'
+]);
+
+
+gulp.task('watch', function(){
+    watch([path.watch.html], function(event, cb) {
+        gulp.start('html:build');
+    });
+    watch([path.watch.style], function(event, cb) {
+        gulp.start('style:build');
+    });
+    watch([path.watch.js], function(event, cb) {
+        gulp.start('js:build');
+    });
+    watch([path.watch.images], function(event, cb) {
+        gulp.start('image:build');
+    });
+    watch([path.watch.fonts], function(event, cb) {
+        gulp.start('fonts:build');
+    });
+    watch([path.watch.sprite], function(event, cb) {
+        gulp.start('sprites');
+    });
+});
+
+gulp.task('webserver', function () {
+    browserSync(config);
+});
+
+gulp.task('clean', function (cb) {
+    rimraf(path.clean, cb);
+});
+
+gulp.task('default', [
+          'build', 
+          // 'webserver',  --нужно сконфигурировать 
+          'watch'
+]);
